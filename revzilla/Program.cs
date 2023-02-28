cd5d5873c702aab9aca2f8613d209a1f797697e8
austi <austi@DESKTOP-STU0DCH>
2023-02-28 11:58:44 -0600

    Add project files.

---
+++ revzilla/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection;
+
+namespace revzilla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cap = 120;
+            int counter = 7;
+
+            while (counter <= cap)
+            {
+                string toPrint = "";
+
+                if (counter % 3 == 0 && counter % 5 == 0 && counter % 7 == 0)
+                {
+                    toPrint += printPizzaParty();
+                }
+
+                if (counter %3 == 0 &&! toPrint.Contains("pizza party"))
+                {
+                    toPrint += printFizz();
+                }
+
+               if(counter % 5 == 0 && !toPrint.Contains("pizza party"))
+                {
+                    toPrint += printBuzz();
+                }
+
+                if (counter % 7 == 0 && !toPrint.Contains("pizza party"))
+                {
+                    toPrint += printPop();
+                }
+
+                if(toPrint == "")
+                {
+                    toPrint = counter.ToString();
+                }
+
+                printCounter(toPrint);
+                counter++;
+            }
+        }
+
+        static string printPizzaParty()
+        {
+            return "pizza party";
+        }
+
+        static string printFizz()
+        {
+            return "fizz";
+        }
+
+        static string printBuzz()
+        {
+            return "buzz";
+
+        }
+        static string printPop()
+        {
+            return "pop";
+        }
+
+        static void printCounter(string toPrint) 
+        {
+            Console.WriteLine(toPrint);
+        }
+    }
+}
